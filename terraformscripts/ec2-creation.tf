6138d227c3803f54d9572225c29fe6c45901a055
PravinRai <kevin11starc@gmail.com>
2023-02-01 17:43:44 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,15 @@
-	provider "aws"{
+provider "aws"{
 	region = "ap-south-1"
 	}
 	
-	resource"aws_instance" "AWSEC2Instance"{
+	resource "aws_instance" "AWSEC2Instance"{
 		count = 1
-		ami = "ami-0f3443fa43a3a92d2"
+		ami = "ami-0f9d9a251c1a44858"
 		instance_type = "t2.micro"
 		key_name= "pravidevopssepbatch2022"
 		security_groups = ["launch-wizard-6"]
 		tags = {
-		Name = "tomcatserver"
+		Name = "TerraformServer"
 		}
 	
 	}
